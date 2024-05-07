404106311aa6faf9dffd6785a43d1e8e2e2a4126
lesponns <adamadakadam1903@gmail.com>
2024-05-07 10:12:40 +0300

    Add project files.

---
+++ project_009/Program.cs
@@ -0,0 +1,12 @@
+﻿Random rnd = new Random();
+
+int[] sides = new int[10], cubeAreas = new int[10];
+Console.WriteLine("Uzunluğu:     Alanı:");
+for (int i = 0; i < sides.Length; i++)
+{
+    sides[i] = rnd.Next(1,10);
+    cubeAreas[i] = sides[i] * sides[i];
+    Console.WriteLine(sides[i] +"     ==>     " + cubeAreas[i]);
+}
+
+Console.ReadKey();
